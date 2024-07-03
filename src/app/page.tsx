923de4e08d732cdd418b94ed17defde808d42175
Victor Marotta <victordossantosmarotta@gmail.com>
2024-07-03 09:33:22 -0300

    feat: add boilerplate code and first texts

---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-centercenter p-24">
+      <div className="flex md:flex-row flex-col md:w-[90%]  justify-items-center justify-center gap-10">
+        <div className="flex flex-row align-top h-full">
+          <img src="me.png" className="w-[150px] object-contain" />
+        </div>
+        <div className="md:w-[50%] w-[95%] flex flex-col gap-4">
+          <p className="text-xl font-semibold">Hello, I'm Victor</p>
+          <p>
+            I'm a full stack developer specializing in geotechnologies. I have
+            extensive experience with major frontend development tools like
+            React, Angular, TypeScript, and Next.js. I'm also proficient in map
+            libraries such as Mapbox, OpenLayers, and Leaflet, and have worked
+            with a variety of backend frameworks including Spring, Play
+            Framework, Django, Express.js, and Nest.js{" "}
+          </p>
+          <p>
+            I graduated with a degree in Surveying and Cartographic Engineering
+            in Universidade Federal de Viçosa, hold a Master's degree in Civil
+            Engineering with a focus on Spatial Information, and I'm currently
+            pursuing a Ph.D. in Civil Engineering, also focusing on Spatial
+            Information{" "}
+          </p>
+          <p>
+            {" "}
+            Right now, I'm working as a requirements analyst at a cartographic
+            engineering company in Brazil, but I'm open to freelancing
+            opportunities both in Brazil and abroad
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
